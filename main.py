5bd4783093b8fd88feb839f127239fb400d56ffc
root <root@localhost.localdomain>
2019-12-14 01:06:07 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.datasets import mnist
+import keras
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+# Get the data
+(x_train, y_train), (x_test,y_test) = mnist.load_data()
+
+#normalize the data [0, 1]
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+#build the model
+model = Sequential()
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+#training
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train,epochs=1)
+
+val_loss, vall_acc = model.evaluate(x_test, y_test)
+
+#testing our model
+model.save('epic_nim_reader.model')
+new_model = tf.keras.models.load_model('epic_nim_reader.model')
+predictions = new_model.predict(x_test)
+print(predictions)
+
+print(np.argmax(predictions[6]))
+
+plt.imshow(x_train[0], cmap = plt.cm.binary)
+plt.show()
+print(x_test[6])
